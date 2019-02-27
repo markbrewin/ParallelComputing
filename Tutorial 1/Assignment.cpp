7a9d2c4fd6adc8329d5a038e94fb0e4c5a56a830
Mark <mark.brewin98@gmail.com>
2019-02-27 14:54:21 +0000

    Initial Commit

---
+++ Tutorial 1/Assignment.cpp
@@ -0,0 +1,74 @@
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#define __CL_ENABLE_EXCEPTIONS
+
+#include <iostream>
+#include <vector>
+
+#ifdef __APPLE__
+#include <OpenCL/cl.hpp>
+#else
+#include <CL/cl.hpp>
+#endif
+
+#include "Utils.h"
+
+void print_help() {
+	std::cerr << "Application usage:" << std::endl;
+
+	std::cerr << "  -p : select platform " << std::endl;
+	std::cerr << "  -d : select device" << std::endl;
+	std::cerr << "  -l : list all platforms and devices" << std::endl;
+	std::cerr << "  -h : print this message" << std::endl;
+}
+
+int main(int argc, char **argv) {
+	//Part 1 - handle command line options such as device selection, verbosity, etc.
+	int platform_id = 0;
+	int device_id = 0;
+
+	for (int i = 1; i < argc; i++)	{
+		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
+		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
+		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
+		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0;}
+	}
+
+	//detect any potential exceptions
+	try {
+		//Part 2 - host operations
+		//2.1 Select computing devices
+		cl::Context context = GetContext(platform_id, device_id);
+
+		//display the selected device
+		std::cout << "Running on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
+
+		//create a queue to which we will push commands for the device
+		cl::CommandQueue queue(context);
+
+		//2.2 Load & build the device code
+		cl::Program::Sources sources;
+
+		AddSources(sources, "kernels.cl");
+
+		cl::Program program(context, sources);
+
+		//build and debug the kernel code
+		try {
+			program.build();
+		}
+		catch (const cl::Error& err) {
+			std::cout << "Build Status: " << program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Options:\t" << program.getBuildInfo<CL_PROGRAM_BUILD_OPTIONS>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			std::cout << "Build Log:\t " << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(context.getInfo<CL_CONTEXT_DEVICES>()[0]) << std::endl;
+			throw err;
+		}
+
+		//Part 3 - memory allocation
+		
+	}
+	catch (cl::Error err) {
+		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
+	}
+
+	return 0;
+}
